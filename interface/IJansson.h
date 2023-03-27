a01a62766d44dfb11aa8ded2c7c7e81fe1f8b831
rejchev <hendlerq@yandex.ru>
2023-03-28 01:37:14 +0500

    added IJsonArray & some methods

--- interface/IJansson.h
+++ interface/IJansson.h
@@ -65,6 +65,14 @@ namespace SourceMod
         IndexOutOfRange
     };
 
+    enum JsonObjectUpdateType
+    {
+        Default,
+        Existing,
+        Missing,
+        Recursive
+    };
+
     struct HandleTypeDetails
     {
         const char *name;
@@ -81,11 +89,8 @@ namespace SourceMod
 
     // TODO: Update, Size, Keys, KeyIterator
     class IJsonObject;
-
-    //    TODO
-//    class IJsonArray;
-
-class IJsonError;
+    class IJsonArray;
+    class IJsonError;
 
     class IJansson : SMInterface
     {
@@ -165,19 +170,48 @@ class IJsonError;
     class IJsonObject : public IJson
     {
         public:
-            virtual IJson   *get(const char *) const =0;
-            virtual bool    set(const char *, const IJson *value) =0;
-            virtual bool    set(const char *, const char* value) =0;
-            virtual bool    set(const char *, double value) =0;
-            virtual bool    set(const char *, bool value) =0;
-            virtual bool    set(const char *, int value) =0;
+            virtual IJson*      get(const char *key) const =0;
+
+        public:
+            virtual bool        set(const char *key, const IJson *value) =0;
+            virtual bool        set(const char *key, const char* value) =0;
+            virtual bool        set(const char *key, double value) =0;
+            virtual bool        set(const char *key, bool value) =0;
+            virtual bool        set(const char *key, int value) =0;
             
         public:
-            virtual JsonType type(const char *) const =0;
-            virtual bool exist(const char *) const =0;
-            virtual bool remove(const char *) =0;
-            virtual bool valid() const =0;
-            virtual void clear() =0;
+            virtual bool        update(const IJsonObject* another, JsonObjectUpdateType type) =0;
+            virtual JsonType    type(const char *key) const =0;
+            virtual bool        exist(const char *key) const =0;
+            virtual bool        remove(const char *key) =0;
+            virtual void        clear() =0;
+    };
+
+    class IJsonArray : public IJson
+    {
+    public:
+        virtual IJson   *get(const size_t& index) const =0;
+
+    public:
+        virtual bool    set(const size_t& index, const IJson *value) =0;
+        virtual bool    set(const size_t& index, const char* value) =0;
+        virtual bool    set(const size_t& index, double value) =0;
+        virtual bool    set(const size_t& index, bool value) =0;
+        virtual bool    set(const size_t& index, int value) =0;
+
+    public:
+        virtual bool    push(const IJson *value) =0;
+        virtual bool    push(const char* value) =0;
+        virtual bool    push(double value) =0;
+        virtual bool    push(bool value) =0;
+        virtual bool    push(int value) =0;
+
+    public:
+        virtual JsonType type(const size_t& index) const =0;
+        virtual bool extend(const IJsonArray *another) =0;
+        virtual bool remove(const size_t& index) =0;
+        virtual size_t length() const =0;
+        virtual void clear() =0;
     };
 };
 
