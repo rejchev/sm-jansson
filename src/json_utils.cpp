8356d9d328de5a2f13e9e6df8bb6b8df8d676534
rejchev <hendlerq@yandex.ru>
2023-04-12 00:36:35 +0500

    missing files added

---
+++ src/json_utils.cpp
@@ -0,0 +1,12 @@
+#include "json_utils.h"
+
+#include <cstring>
+
+namespace nJansson {
+    size_t json_string_size(const json_t *obj) {
+        const char* value = json_string_value(obj);
+
+        return ((value == nullptr) ? 0 : strlen(value));
+    }
+
+} // nJansson
---
+++ src/json_utils.h
@@ -0,0 +1,12 @@
+#ifndef SM_JANSSON_JSON_UTILS_H
+#define SM_JANSSON_JSON_UTILS_H
+
+#include <jansson.h>
+
+namespace nJansson {
+
+    extern size_t json_string_size(const json_t*);
+
+} // nJansson
+
+#endif //SM_JANSSON_JSON_UTILS_H
