a593cedc6b7b9f65d21ea8f3eecedd391af2165e
rejchev <hendlerq@yandex.ru>
2026-08-04 13:23:37 +0500

    rem: jansson

--- jansson/private_include/jansson_private_config.h
+++
@@ -1,7 +0,0 @@
-#ifdef __linux__
-# include "jansson_private_config-linux.h"
-#elif defined(__APPLE__)
-# include "jansson_private_config-mac.h"
-#elif defined(WIN32)
-# include "jansson_private_config-windows.h"
-#endif
